7682664897efce7c532262944775fa4569d98968
Hakan <durmazhakan@icloud.com>
2021-07-24 12:59:07 +0300

    Add project files.

---
+++ mybooks/Data/AppDbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using mybooks.Data.Models;
+
+namespace mybooks.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book()
+                        {
+                            Title = "1st book",
+                            Description = "1st book description",
+                            IsRead = true,
+                            Genre = "Biography",
+                            CoverUrl = "https...",
+                            DateAdded = DateTime.Now
+                        },new Book()
+                        {
+                            Title = "2st book",
+                            Description = "2st book description",
+                            IsRead = false,
+                            Genre = "Biography",
+                            CoverUrl = "https...",
+                            DateAdded = DateTime.Now
+                        });
+
+                    context.SaveChanges();
+
+                }
+            }
+        }
+    }
+}
---
+++ mybooks/Migrations/20210723145345_removecolumn.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mybooks.Migrations
+{
+    public partial class removecolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
